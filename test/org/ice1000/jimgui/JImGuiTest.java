f87a1a6fd82c8ae7c5c5d477791512e5604054d9
ice1000 <ice1000kotlin@foxmail.com>
2018-05-01 02:30:53 +0800

    Make test work

--- test/org/ice1000/jimgui/JImGuiTest.java
+++ test/org/ice1000/jimgui/JImGuiTest.java
@@ -3,6 +3,7 @@ package org.ice1000.jimgui;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
 import java.nio.file.Paths;
 
 import static org.junit.Assert.assertTrue;
@@ -16,8 +17,10 @@ public class JImGuiTest {
 	@Test
 	public void jniInitialization() throws NoSuchFieldException, IllegalAccessException {
 		try (JImGui imGui = new JImGui()) {
-			long nativeObjectPtr = (long) JImGui.class.getDeclaredField("nativeObjectPtr").get(imGui);
-			assertTrue(nativeObjectPtr != 0);
+			Field nativeObjectPtr = JImGui.class.getDeclaredField("nativeObjectPtr");
+			nativeObjectPtr.setAccessible(true);
+			assertTrue((long) nativeObjectPtr.get(imGui) != 0);
+			nativeObjectPtr.setAccessible(false);
 		}
 	}
 }
