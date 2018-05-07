bee08d6ef57ddac64e82ad8f0091bae01743476b
ice1000 <ice1000@sbrella.com>
2018-05-07 16:51:03 +0800

    Use macro to generate code

--- core/jni/trivial_impl.cpp
+++ core/jni/trivial_impl.cpp
@@ -11,6 +11,7 @@
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+
 jfloat Java_org_ice1000_jimgui_JImGuiIO_getMousePosX(JNIEnv *, jobject) {
 	return ImGui::GetMousePos().x;
 }
@@ -64,92 +65,34 @@ void Java_org_ice1000_jimgui_MutableJImVec4_setW(JNIEnv *, jclass, jlong nativeO
 	reinterpret_cast<ImVec4 *> (nativeObjectPtr)->w = newValue;
 }
 
-jint Java_org_ice1000_jimgui_JImStyleVar_getAlpha(JNIEnv *, jclass) {
-	return ImGuiStyleVar_Alpha;
+#define JImStyleVarFunctionDefinition(variableName) \
+jint Java_org_ice1000_jimgui_JImStyleVar_get ## variableName(JNIEnv *, jclass) { \
+  return ImGuiStyleVar_ ## variableName; \
 }
 
-jint Java_org_ice1000_jimgui_JImStyleVar_getWindowPadding(JNIEnv *, jclass) {
-	return ImGuiStyleVar_WindowPadding;
-}
+JImStyleVarFunctionDefinition(Alpha)
+JImStyleVarFunctionDefinition(WindowPadding)
+JImStyleVarFunctionDefinition(WindowRounding)
+JImStyleVarFunctionDefinition(WindowBorderSize)
+JImStyleVarFunctionDefinition(WindowMinSize)
+JImStyleVarFunctionDefinition(WindowTitleAlign)
+JImStyleVarFunctionDefinition(ChildRounding)
+JImStyleVarFunctionDefinition(ChildBorderSize)
+JImStyleVarFunctionDefinition(PopupRounding)
+JImStyleVarFunctionDefinition(PopupBorderSize)
+JImStyleVarFunctionDefinition(FramePadding)
+JImStyleVarFunctionDefinition(FrameRounding)
+JImStyleVarFunctionDefinition(FrameBorderSize)
+JImStyleVarFunctionDefinition(ItemSpacing)
+JImStyleVarFunctionDefinition(ItemInnerSpacing)
+JImStyleVarFunctionDefinition(IndentSpacing)
+JImStyleVarFunctionDefinition(ScrollbarSize)
+JImStyleVarFunctionDefinition(ScrollbarRounding)
+JImStyleVarFunctionDefinition(GrabMinSize)
+JImStyleVarFunctionDefinition(GrabRounding)
+JImStyleVarFunctionDefinition(ButtonTextAlign)
+JImStyleVarFunctionDefinition(COUNT)
 
-jint Java_org_ice1000_jimgui_JImStyleVar_getWindowRounding(JNIEnv *, jclass) {
-	return ImGuiStyleVar_WindowRounding;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getWindowBorderSize(JNIEnv *, jclass) {
-	return ImGuiStyleVar_WindowBorderSize;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getWindowMinSize(JNIEnv *, jclass) {
-	return ImGuiStyleVar_WindowMinSize;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getWindowTitleAlign(JNIEnv *, jclass) {
-	return ImGuiStyleVar_WindowTitleAlign;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getChildRounding(JNIEnv *, jclass) {
-	return ImGuiStyleVar_ChildRounding;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getChildBorderSize(JNIEnv *, jclass) {
-	return ImGuiStyleVar_ChildBorderSize;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getPopupRounding(JNIEnv *, jclass) {
-	return ImGuiStyleVar_PopupRounding;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getPopupBorderSize(JNIEnv *, jclass) {
-	return ImGuiStyleVar_PopupBorderSize;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getFramePadding(JNIEnv *, jclass) {
-	return ImGuiStyleVar_FramePadding;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getFrameRounding(JNIEnv *, jclass) {
-	return ImGuiStyleVar_FrameRounding;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getFrameBorderSize(JNIEnv *, jclass) {
-	return ImGuiStyleVar_FrameBorderSize;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getItemSpacing(JNIEnv *, jclass) {
-	return ImGuiStyleVar_ItemSpacing;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getItemInnerSpacing(JNIEnv *, jclass) {
-	return ImGuiStyleVar_ItemInnerSpacing;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getIndentSpacing(JNIEnv *, jclass) {
-	return ImGuiStyleVar_IndentSpacing;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getScrollbarSize(JNIEnv *, jclass) {
-	return ImGuiStyleVar_ScrollbarSize;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getScrollbarRounding(JNIEnv *, jclass) {
-	return ImGuiStyleVar_ScrollbarRounding;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getGrabMinSize(JNIEnv *, jclass) {
-	return ImGuiStyleVar_GrabMinSize;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getGrabRounding(JNIEnv *, jclass) {
-	return ImGuiStyleVar_GrabRounding;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getButtonTextAlign(JNIEnv *, jclass) {
-	return ImGuiStyleVar_ButtonTextAlign;
-}
-
-jint Java_org_ice1000_jimgui_JImStyleVar_getCOUNT(JNIEnv *, jclass) {
-	return ImGuiStyleVar_COUNT;
-}
+#undef JImStyleVarFunctionDefinition
 
 #pragma clang diagnostic pop
