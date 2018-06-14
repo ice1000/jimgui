0d4bedb24aef9623658cf5d4d35dff53cd4a48af
ice1000 <ice1000@sbrella.com>
2018-06-14 14:03:00 +0800

    Update demo

--- core/test/org/ice1000/jimgui/tests/EditorTest.java
+++ core/test/org/ice1000/jimgui/tests/EditorTest.java
@@ -7,6 +7,8 @@ import org.jetbrains.annotations.NotNull;
 import java.net.URISyntaxException;
 
 public class EditorTest {
+	public static final int LIMIT = 3;
+
 	@SuppressWarnings("AccessStaticViaInstance")
 	public static void main(String @NotNull ... args) throws InterruptedException, URISyntaxException {
 		JniLoader.load();
@@ -16,34 +18,42 @@ public class EditorTest {
 			JImGuiIO io = gui.getIO();
 			JImFontAtlas fonts = io.getFonts();
 			String fontPath = EditorTest.class.getResource("/font/sarasa-gothic-sc-regular.ttf").toURI().getPath();
-			JImFont sarasaGothic = fonts.addFontFromFile(fontPath, 20, fonts.getGlyphRangesForChineseSimplifiedCommon());
 			int texHeight = 20;
+			JImFont sarasaGothic = fonts.addFontFromFile(fontPath, texHeight, fonts.getGlyphRangesForChineseSimplifiedCommon());
+			System.out.println(texHeight);
+			long lastMovedTime = 0;
 			JImStyle style = gui.getStyle();
-			style.setItemSpacingX(1f);
+			style.setItemSpacingX(2f);
 			while (!gui.windowShouldClose()) {
-				float deltaTime = io.getDeltaTime() * 1000;
-				Thread.sleep((long) Math.abs(20 - deltaTime));
+				long deltaTime = (long) (io.getDeltaTime() * 1000);
+				Thread.sleep(LIMIT - deltaTime < 0 ? 0 : (LIMIT - deltaTime));
 				gui.initNewFrame();
 				char[] inputString = io.getInputChars();
 				io.clearInputCharacters();
 				builder.insert(cursor, inputString);
 				cursor += inputString.length;
-				if (gui.isKeyPressed(io.keyMapAt(JImDefaultKeys.LeftArrow)) && cursor > 0) cursor--;
-				if (gui.isKeyPressed(io.keyMapAt(JImDefaultKeys.RightArrow)) && cursor < builder.length()) cursor++;
+				if (gui.isKeyPressed(io.keyMapAt(JImDefaultKeys.LeftArrow)) && cursor > 0) {
+					lastMovedTime = System.currentTimeMillis();
+					cursor--;
+				}
+				if (gui.isKeyPressed(io.keyMapAt(JImDefaultKeys.RightArrow)) && cursor < builder.length()) {
+					lastMovedTime = System.currentTimeMillis();
+					cursor++;
+				}
 				if (gui.isKeyPressed(io.keyMapAt(JImDefaultKeys.Enter))) builder.insert(cursor++, '\n');
 				if (gui.isKeyPressed(io.keyMapAt(JImDefaultKeys.Backspace)) && cursor > 0) builder.deleteCharAt(--cursor);
 				gui.begin("Editor");
 				int length = builder.length();
 				for (int i = 0; i <= length; i++) {
-					if (i == cursor && (System.currentTimeMillis() / 1500) % 2 == 1) {
-						float cursorPosX = gui.getCursorPosX() + gui.getWindowPosX() - 2;
+					if (i == cursor && ((System.currentTimeMillis() - lastMovedTime) / 500) % 2 == 0) {
+						float cursorPosX = gui.getCursorPosX() + gui.getWindowPosX() - 1;
 						float cursorPosY = gui.getCursorPosY() + gui.getWindowPosY();
 						gui.getWindowDrawList().addLine(cursorPosX,
 								cursorPosY,
 								cursorPosX,
 								cursorPosY + texHeight,
 								0xffffffff,
-								2);
+								1.5f);
 					}
 					if (i == length) break;
 					char c = builder.charAt(i);
