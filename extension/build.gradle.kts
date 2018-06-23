e0b950b98f7d7b337e56d72770d8593a76c79afd
ice1000 <ice1000kotlin@foxmail.com>
2018-06-24 04:42:30 +0800

    Add test

--- extension/build.gradle.kts
+++ extension/build.gradle.kts
@@ -4,6 +4,10 @@ java.sourceSets {
 		java.setSrcDirs(listOf("src"))
 		resources.setSrcDirs(listOf("res"))
 	}
+	"test" { java.setSrcDirs(listOf("test")) }
 }
 repositories { jcenter() }
-dependencies { compile(project(":core")) }
+dependencies {
+	compile(project(":core"))
+	testCompile(group = "junit", name = "junit", version = "4.12")
+}
---
+++ extension/test/org/ice1000/jimgui/util/JniLoaderExTest.java
@@ -0,0 +1,23 @@
+package org.ice1000.jimgui.util;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assume.assumeFalse;
+
+public class JniLoaderExTest {
+	@BeforeClass
+	public static void setup() {
+		assumeFalse("true".equalsIgnoreCase(System.getenv("CI")));
+		assumeFalse("true".equalsIgnoreCase(System.getenv("APPVEYOR")));
+		assumeFalse("true".equalsIgnoreCase(System.getProperty("java.awt.headless")));
+	}
+
+	@Test
+	public void testInit() {
+		JniLoaderEx.loadGlfw();
+		JImGuiUtil.runWithinPer(8000, 15, imGui -> {
+			imGui.text("Hello World");
+		});
+	}
+}
