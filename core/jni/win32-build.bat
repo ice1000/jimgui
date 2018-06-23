4793be4570444ca2f48c44fca8865cc4fb68bc27
ice1000 <ice1000kotlin@foxmail.com>
2018-06-24 01:02:57 +0800

    Updated build script

--- core/jni/win32-build.bat
+++ core/jni/win32-build.bat
@@ -1,8 +1,7 @@
 @mkdir win32-build
-@cd src
-@set JDK_PATH=C:\Program Files\Java\jdk1.8.0_102
+@set JDK_PATH=C:\Program Files\Java\jdk-10.0.1
 @set LD_OPTION=-lglfw3 -lgdi32 -lopengl32
 @set INCLUDE_OPTION=-I . -I .\javah  -I .\impl -I .\imgui -I .\config -I "%JDK_PATH%\include" -I "%JDK_PATH%\include\win32"
-@gcc -c %INCLUDE_OPTION% .\impl\gl3w.c -o ..\win32-build\gl3w.o
-@g++ -std=c++11 -shared -static -fPIC -s -O2 %INCLUDE_OPTION% ..\win32-build\gl3w.o .\generated*.cpp .\hand_written_bindings.cpp .\overloads_helper.cpp .\cpp_interop.cpp .\glfw_impl.cpp .\imgui\*.cpp .\impl\*.cpp -o ..\win32-build\jimgui-glfw.dll %LD_OPTION%
-@del /F /Q ..\win32-build\*.o
+@gcc -c %INCLUDE_OPTION% .\impl\gl3w.c -o .\win32-build\gl3w.o
+@g++ -std=c++11 -shared -static -fPIC -s -O2 %INCLUDE_OPTION% .\win32-build\gl3w.o .\generated*.cpp .\hand_written_bindings.cpp .\overloads_helper.cpp .\cpp_interop.cpp .\glfw_impl.cpp .\imgui\*.cpp .\impl\*.cpp -o .\win32-build\jimgui-glfw.dll %LD_OPTION%
+@del /F /Q .\win32-build\*.o
