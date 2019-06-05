c2ce7b7ee9f6ff5f296d70644c44da198a80e0c2
ice1000 <ice1000kotlin@foxmail.com>
2019-06-05 15:59:26 -0700

    [ #33 ] Use a cmake project

--- core/jni/win32-build.bat
+++
@@ -1,9 +0,0 @@
-@mkdir win32-build
-@set LD_OPTION=-lglfw3 -lgdi32 -lopengl32
-@set COMPILE_OPTION=-std=c++11 -shared -static -fPIC -s -O2
-@set SOURCE=.\win32-build\gl3w.o .\generated*.cpp .\hand_written_bindings.cpp .\overloads_helper.cpp .\cpp_interop.cpp .\glfw_impl.cpp .\imgui\*.cpp .\impl\*.cpp
-@set INCLUDE_OPTION=-I . -I .\javah  -I .\impl -I .\imgui -I .\config -I "%JAVA_HOME%\include" -I "%JAVA_HOME%\include\win32"
-@gcc -c %INCLUDE_OPTION% .\impl\gl3w.c -o .\win32-build\gl3w.o
-@g++ %COMPILE_OPTION% %INCLUDE_OPTION% %SOURCE% -o .\win32-build\jimgui-glfw.dll %LD_OPTION%
-@g++ %COMPILE_OPTION% -m32 %INCLUDE_OPTION% %SOURCE% -o .\win32-build\jimgui32-glfw.dll %LD_OPTION%
-@del /F /Q .\win32-build\*.o
