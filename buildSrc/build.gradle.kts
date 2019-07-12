ec262560409af69e332fa97cef639db8da22d9eb
ice1000 <ice1000kotlin@foxmail.com>
2019-07-12 06:25:46 -0400

    [ build ] Use Kotlin 1.3.40

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 group = "org.ice1000.gradle"
 version = "v0.1"
 
-plugins { kotlin("jvm") version "1.3.30" }
+plugins { kotlin("jvm") version "1.3.40" }
 
 sourceSets {
 	main {
