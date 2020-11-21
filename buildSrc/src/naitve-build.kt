9a61b93a9548f0afeac51cee06e8503c6bec6768
ice1000 <ice1000kotlin@foxmail.com>
2020-11-21 09:12:17 +0800

    [ build ] Create CxxCompile and CMake tasks

---
+++ buildSrc/src/naitve-build.kt
@@ -0,0 +1,49 @@
+package org.ice1000.gradle
+
+import org.apache.tools.ant.taskdefs.condition.Os
+import org.gradle.api.tasks.Exec
+import java.io.File
+
+private val nativeLibraryExtensions = listOf("so", "dll", "dylib")
+val isWindows = Os.isFamily(Os.FAMILY_WINDOWS)
+
+abstract class NativeBuildTask : Exec() {
+	lateinit var jniDir: File
+	lateinit var resDir: File
+
+	init {
+		group = "native compile"
+	}
+}
+
+open class CMake : NativeBuildTask() {
+	fun cmake(workingDir: File, generator: String, vararg additional: String) {
+		workingDir(workingDir)
+		outputs.dir(workingDir)
+		inputs.dir(jniDir.resolve("imgui"))
+		inputs.dir(jniDir.resolve("impl"))
+		inputs.file(jniDir.resolve("CMakeLists.txt"))
+		commandLine("cmake", "-DCMAKE_BUILD_TYPE=Release", "-G", generator, *additional, workingDir.parent)
+		doFirst { workingDir.mkdirs() }
+	}
+}
+
+open class CxxCompile : NativeBuildTask() {
+	// @Finalize
+	fun findNativeLibs() = workingDir.walk()
+			.filter { it.extension in nativeLibraryExtensions }
+			.forEach {
+				println("Found native library $it")
+				it.copyTo(resDir.resolve("native").resolve(it.name), overwrite = true)
+			}
+
+	fun cxx(workingDir: File, vararg commandLine: String) {
+		workingDir(workingDir)
+		commandLine(*commandLine)
+		outputs.dir(workingDir)
+		inputs.files(jniDir.listFiles().orEmpty().filter { it.name.endsWith("cpp") })
+		inputs.dir(jniDir.resolve("imgui"))
+		inputs.dir(jniDir.resolve("impl"))
+		doLast { findNativeLibs() }
+	}
+}
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,19 +1,25 @@
 import de.undercouch.gradle.tasks.download.Download
 import org.apache.tools.ant.taskdefs.condition.Os
 import org.ice1000.gradle.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	java
 	id("de.undercouch.download")
 }
 
-val jniDir = projectDir.resolve("jni").absoluteFile!!
-val imguiDir = jniDir.resolve("imgui")
-val implDir = jniDir.resolve("impl")
-val `cmake-build-win64` = jniDir.resolve("cmake-build-win64")
-val `cmake-build` = jniDir.resolve("cmake-build")
-val javahDir = jniDir.resolve("javah")
+val jni = projectDir.resolve("jni").absoluteFile
+val imguiDir = jni.resolve("imgui")
+val implDir = jni.resolve("impl")
+val `cmake-build-win64` = jni.resolve("cmake-build-win64")
+val `cmake-build` = jni.resolve("cmake-build")
+val javahDir = jni.resolve("javah")
 val res = projectDir.resolve("res")
+
+fun NativeBuildTask.preconfigure() {
+	jniDir = jni
+	resDir = res
+}
 
 tasks.compileJava {
 	options.compilerArgs = listOf("-h", "$javahDir")
@@ -22,44 +28,13 @@ tasks.compileJava {
 val clean = tasks.named<Delete>("clean")
 val cleanGroup = clean.get().group
 val downloadGroup = "download"
-val compileGroup = "native compile"
 val downloadAll = tasks.register("downloadAll") {
 	group = downloadGroup
 	description = "Virtual task representing all downloading tasks"
 }
 val compileCxx = tasks.register("compileCxx") {
-	group = compileGroup
+	group = "native compile"
 	description = "Virtual task representing all C++ compilation tasks"
-}
-
-val nativeLibraryExtensions = listOf("so", "dll", "dylib")
-fun Exec.configureCxxBuild(workingDir: File, vararg commandLine: String) {
-	group = compileGroup
-	workingDir(workingDir)
-	commandLine(*commandLine)
-	outputs.dir(workingDir)
-	inputs.files(jniDir.listFiles().orEmpty().filter { it.name.endsWith("cpp") })
-	inputs.dir(jniDir.resolve("imgui"))
-	inputs.dir(jniDir.resolve("impl"))
-	doLast {
-		workingDir.walk()
-			.filter { it.extension in nativeLibraryExtensions }
-			.forEach {
-				println("Found native library $it")
-				it.copyTo(res.resolve("native").resolve(it.name), overwrite = true)
-			}
-	}
-}
-
-fun Exec.configureCMake(workingDir: File, generator: String, vararg additional: String) {
-	group = compileGroup
-	workingDir(workingDir)
-	outputs.dir(workingDir)
-	inputs.dir(jniDir.resolve("imgui"))
-	inputs.dir(jniDir.resolve("impl"))
-	inputs.file(jniDir.resolve("CMakeLists.txt"))
-	commandLine("cmake", "-DCMAKE_BUILD_TYPE=Release", "-G", generator, *additional, workingDir.parent)
-	doFirst { workingDir.mkdirs() }
 }
 
 val genImgui = tasks.register<GenGenTask>("genImgui") {
@@ -136,39 +111,43 @@ val downloadIce1000 = tasks.register<Download>("downloadIce1000") {
 	overwrite(false)
 }
 
-val isWindows = Os.isFamily(Os.FAMILY_WINDOWS)
-val cmakeWin64 = tasks.register<Exec>("cmakeWin64") {
+val cmakeWin64 = tasks.register<CMake>("cmakeWin64") {
+	preconfigure()
 	if (isWindows)
-		configureCMake(`cmake-build-win64`, "Visual Studio 16 2019", "-A", "x64")
-	else configureCMake(`cmake-build-win64`, "Unix Makefiles")
+		cmake(`cmake-build-win64`, "Visual Studio 16 2019", "-A", "x64")
+	else cmake(`cmake-build-win64`, "Unix Makefiles")
 	dependsOn(tasks.compileJava, downloadAll)
 }
 
-val cmake = tasks.register<Exec>("cmake") {
+val cmake = tasks.register<CMake>("cmake") {
+	preconfigure()
 	if (isWindows)
-		configureCMake(`cmake-build`, "Visual Studio 16 2019", "-A", "Win32")
-	else configureCMake(`cmake-build`, "Unix Makefiles")
+		cmake(`cmake-build`, "Visual Studio 16 2019", "-A", "Win32")
+	else cmake(`cmake-build`, "Unix Makefiles")
 	dependsOn(tasks.compileJava, downloadAll)
 }
 
-val make = tasks.register<Exec>("make") {
+val make = tasks.register<CxxCompile>("make") {
+	preconfigure()
 	dependsOn(cmake)
-	configureCxxBuild(`cmake-build`, "make", "-f", "Makefile")
+	cxx(`cmake-build`, "make", "-f", "Makefile")
 }
 
-val msbuild = tasks.register<Exec>("msbuild") {
+val msbuild = tasks.register<CxxCompile>("msbuild") {
+	preconfigure()
 	dependsOn(cmake)
-	configureCxxBuild(`cmake-build`, "msbuild", "jimgui.sln", "/t:Build", "/p:Configuration=Release")
+	cxx(`cmake-build`, "msbuild", "jimgui.sln", "/t:Build", "/p:Configuration=Release")
 }
 
-val msbuildWin64 = tasks.register<Exec>("msbuildWin64") {
+val msbuildWin64 = tasks.register<CxxCompile>("msbuildWin64") {
+	preconfigure()
 	dependsOn(cmakeWin64)
-	configureCxxBuild(`cmake-build-win64`, "msbuild", "jimgui.sln", "/t:Build", "/p:Configuration=Release")
+	cxx(`cmake-build-win64`, "msbuild", "jimgui.sln", "/t:Build", "/p:Configuration=Release")
 }
 
 val clearGenerated = tasks.register<Delete>("clearGenerated") {
 	group = cleanGroup
-	delete(projectDir.resolve("gen"), javahDir, *jniDir.listFiles { f: File -> f.name.startsWith("generated") }.orEmpty())
+	delete(projectDir.resolve("gen"), javahDir, *jni.listFiles { f: File -> f.name.startsWith("generated") }.orEmpty())
 }
 
 val clearCMake = tasks.register<Delete>("clearCMake") {
@@ -185,9 +164,19 @@ downloadAll.configure {
 	dependsOn(downloadImplGL, downloadImpl, downloadImgui)
 }
 tasks.compileJava {
-	dependsOn(genImguiIO, genImguiFont, genImguiStyle, genImgui, genImguiDrawList,
-		genNativeTypes, genImguiStyleVar, genImguiDefaultKeys,
-		genImguiStyleColor, genImguiFontAtlas, genImguiFontConfig)
+	dependsOn(
+			genImguiIO,
+			genImguiFont,
+			genImguiStyle,
+			genImgui,
+			genImguiDrawList,
+			genNativeTypes,
+			genImguiStyleVar,
+			genImguiDefaultKeys,
+			genImguiStyleColor,
+			genImguiFontAtlas,
+			genImguiFontConfig
+	)
 }
 
 clean.configure {
