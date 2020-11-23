a7bc20521ebd030234da47101c2ff18eca6bb84b
ice1000 <ice1000kotlin@foxmail.com>
2020-11-24 06:27:13 +0800

    [ party ] Two more: matrix effect and thunder storm

---
+++ core/test/org/ice1000/jimgui/dlparty/MatrixEffect.java
@@ -0,0 +1,62 @@
+package org.ice1000.jimgui.dlparty;
+
+import org.ice1000.jimgui.JImDrawList;
+import org.jetbrains.annotations.NotNull;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
+public class MatrixEffect implements TestBed {
+	static int S = 0x1234;
+	static float t0 = -1;
+
+	static class MM {
+		int y, h, c;
+		float t, s;
+	}
+
+	MM[] m = new MM[40];
+
+	{
+		for (int i = 0; i < m.length; i++) m[i] = new MM();
+	}
+
+	@Override
+	public void fx(@NotNull JImDrawList d, ImVec2 a, ImVec2 b, ImVec2 sz, ImVec4 mouse, float t) {
+		if (t0 == -1) t0 = t;
+		float ZI = t * .07f, Z = ZI + 1.f;
+		for (int x = 0; x < 40; x++) {
+			MM M = m[x];
+			boolean i = x >= 15 && x < 25;
+			if (M.s == 0.f || M.y > 16) {
+				M.h = b2i(t < 7.f || i) * ((int) (2 + t * .5f) + S % (int) (6 + (t * 0.3f)));
+				M.y = b2i(M.s == 0.f) * -(S % 15) - M.h;
+				M.c += S;
+				M.s = (5 + (S % 14)) * (.01f - t * .001f);
+				if (t > 5.f && i) {
+					M.c = (int) ((340003872375972L >> (x * 5 - 75)) & 31);
+					M.h = b2i(x != 19);
+				}
+			}
+			if ((M.t -= t - t0) < 0.f) {
+				if (t < 6.f || !i || M.y != 6) M.y++;
+				M.t += M.s;
+			}
+			char c = (char) (64 | M.c % 42);
+			for (int j = 0; j < M.h; j++, c = (char) (64 | (c ^ M.c + M.h ^ j) % 42))
+				for (int f = (j + 1 == M.h) ? 13 : 4 + (M.c & 1); f-- != 0; )
+					d.addText(13 * (i ? Z : -Z), a.x - (sz.x * .5f * ZI) + x * 8 * Z + (float) sin(j + t * f), a.y - (sz.y * .5f * ZI) + (M.y + j) * 13 * Z + (float) cos(x * f - t), 0x3c68bb5b, String.valueOf(c));
+			S |= ((S & 1) ^ ((S & 8) >> 2)) << 16;
+			S >>= 1;
+		}
+		t0 = t;
+	}
+
+	private int b2i(boolean b) {
+		return b ? 1 : 0;
+	}
+
+	public static void main(String[] args) {
+		new MatrixEffect().launch();
+	}
+}
---
+++ core/test/org/ice1000/jimgui/dlparty/ThunderStorm.java
@@ -0,0 +1,43 @@
+package org.ice1000.jimgui.dlparty;
+
+import org.ice1000.jimgui.JImDrawList;
+import org.ice1000.jimgui.JImGuiGen;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+import static java.lang.Math.*;
+
+public class ThunderStorm extends JImGuiGen implements TestBed {
+	private float fl;
+	private final Random random = new Random(System.currentTimeMillis());
+
+	@Override
+	public void fx(@NotNull JImDrawList d, ImVec2 a, ImVec2 b, ImVec2 sz, ImVec4 mouse, float t) {
+		if (random.nextInt(500) == 0) fl = t;
+		if (t - fl > 0) {
+			float ft = 0.25f;
+			d.addRectFilled(a.x, a.y, b.x, b.y, white((ft - (t - fl)) / ft));
+		}
+
+		for (int i = 0; i < 2000; ++i) {
+			int h = getID(String.valueOf(i + (int) (t / 4)));
+			float f = (t + ((h / 777.f) % 99)) % 99;
+			int tx = h % (int) sz.x;
+			int ty = h % (int) sz.y;
+			if (f < 1) {
+				float py = ty - 1000 * (1 - f);
+				d.addLine(a.x + tx, a.y + py, a.x + tx, a.y + min(py + 10, ty),  -1);
+			} else if (f < 1.2f)
+				d.addCircle(a.x + tx, a.y + ty, (f - 1) * 10 + h % 5, white(1 - (f - 1) * 5.f));
+		}
+	}
+
+	private int white(float wh) {
+		return IM_COL32(255, 255, 255, 255 * Math.max(wh, 0));
+	}
+
+	public static void main(String[] args) {
+		new ThunderStorm().launch();
+	}
+}
