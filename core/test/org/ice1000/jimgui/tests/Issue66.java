d3ab334823f10df6b9eda8e83478db9e54e08d15
ice1000 <ice1000kotlin@foxmail.com>
2020-12-13 16:23:03 +0800

    [ #66 ] Test memory leak

---
+++ core/test/org/ice1000/jimgui/tests/Issue66.java
@@ -0,0 +1,30 @@
+package org.ice1000.jimgui.tests;
+
+import org.ice1000.jimgui.JImGui;
+import org.ice1000.jimgui.NativeBool;
+import org.ice1000.jimgui.util.JniLoader;
+
+public class Issue66 {
+  public static void main(String[] args) {
+    JniLoader.load();
+    try (JImGui imgui = new JImGui(); NativeBool bool = new NativeBool()) {
+      byte[] b = new byte[100];
+      imgui.initBeforeMainLoop();
+      while (!imgui.windowShouldClose()) {
+        imgui.initNewFrame();
+        imgui.checkbox("Check to test input, uncheck to test ID", bool);
+        if (imgui.button("System.gc()")) System.gc();
+        for (int i = 0; i < 50; i++) {
+          if (bool.accessValue()) {
+            imgui.inputText("Label " + i, b);
+          } else {
+            imgui.pushID("btn" + i);
+            imgui.button("OK");
+            imgui.popID();
+          }
+        }
+        imgui.render();
+      }
+    }
+  }
+}
