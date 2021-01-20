27f7e1828d319150217dca52bbac858381c84ec9
ice1000 <ice1000kotlin@foxmail.com>
2021-01-20 21:49:12 +0800

    [ test ] A test is a test.

--- core/test/org/ice1000/jimgui/features/SetWindowTitle.java
+++ core/test/org/ice1000/jimgui/features/SetWindowTitle.java
@@ -3,8 +3,19 @@ package org.ice1000.jimgui.features;
 import org.ice1000.jimgui.NativeString;
 import org.ice1000.jimgui.util.JImGuiUtil;
 import org.ice1000.jimgui.util.JniLoader;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.ice1000.jimgui.tests.JImGuiTest.useAlternativeJniLibAndCheckHeadless;
 
 public class SetWindowTitle {
+  @BeforeClass public static void setup() {
+    useAlternativeJniLibAndCheckHeadless();
+  }
+  @Test public void test() {
+    main();
+  }
+
   public static void main(String... args) {
     JniLoader.load();
     NativeString string = new NativeString();
